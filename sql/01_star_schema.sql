68c20e414e8d7efe8950b764dec68dae2510c207
adrymmm <233598406+adrymmm@users.noreply.github.com>
2026-09-17 21:25:11 +0300

    initial staging schema

---
+++ sql/01_star_schema.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE land_registry;
+
+CREATE TABLE raw_price_paid (
+    transaction_id  text,
+    price           text,
+    date_of_transfer text,
+    postcode        text,
+    property_type   text,
+    old_new         text,
+    duration        text,
+    paon            text,
+    saon            text,
+    street          text,
+    locality        text,
+    town_city       text,
+    district        text,
+    county          text,
+    ppd_category    text,
+    record_status   text
+);
